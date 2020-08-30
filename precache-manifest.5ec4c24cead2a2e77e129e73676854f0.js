dbfa7c28f92105ef820a4e20bfc78cf7e05270c7
folkomachtsachen <70347339+folkomachtsachen@users.noreply.github.com>
2020-08-31 01:33:22 +0200

    Updates

---
+++ precache-manifest.5ec4c24cead2a2e77e129e73676854f0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2cc090d33a60ad3982b49a1e9fcd14",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "5b565af7bf9bb3944bef",
+    "url": "/shop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa68a7ecd52871b9283e",
+    "url": "/shop/static/js/2.b96033a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shop/static/js/2.b96033a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b565af7bf9bb3944bef",
+    "url": "/shop/static/js/main.b0980f89.chunk.js"
+  },
+  {
+    "revision": "aa1945b71dc1f6fb53f2",
+    "url": "/shop/static/js/runtime-main.4a8e269c.js"
+  },
+  {
+    "revision": "40c7987aa6f63efca3086b7fc44d36fa",
+    "url": "/shop/static/media/aceton.40c7987a.jpg"
+  },
+  {
+    "revision": "e46210f493ada9d66871a20e98b544db",
+    "url": "/shop/static/media/arrow_back.e46210f4.svg"
+  },
+  {
+    "revision": "51a04ea87fd8c49bac3dec851b9b8d02",
+    "url": "/shop/static/media/auto.51a04ea8.jpg"
+  },
+  {
+    "revision": "ca8300db35a97e7939550586f2932df5",
+    "url": "/shop/static/media/bohrer.ca8300db.jpg"
+  },
+  {
+    "revision": "0737e8fecb0bb6bc2f7fc500c430aed5",
+    "url": "/shop/static/media/cart.0737e8fe.svg"
+  },
+  {
+    "revision": "3ae556005b37a7e0b0ddb39a335b18b3",
+    "url": "/shop/static/media/chess.3ae55600.jpg"
+  },
+  {
+    "revision": "d6e2feb285231fa5b1ca63dd378ed782",
+    "url": "/shop/static/media/eimer.d6e2feb2.jpg"
+  },
+  {
+    "revision": "55df8864e5f468bbbed7041009fb9eaa",
+    "url": "/shop/static/media/icon_home.55df8864.svg"
+  },
+  {
+    "revision": "02262a1d7cb87c41bfba1f74f5dbb28e",
+    "url": "/shop/static/media/logo.02262a1d.svg"
+  },
+  {
+    "revision": "accd3003af847e6ba95e742cacc11cd7",
+    "url": "/shop/static/media/lupe.accd3003.jpg"
+  },
+  {
+    "revision": "bd96a1c442d49afa2da63f33800d0bcd",
+    "url": "/shop/static/media/nasenhaarpflege.bd96a1c4.jpg"
+  },
+  {
+    "revision": "c121fa38cd0a272929e958cd2692ce47",
+    "url": "/shop/static/media/ronny.c121fa38.jpg"
+  },
+  {
+    "revision": "37ab2216b8a3fe09abc3113816d4932d",
+    "url": "/shop/static/media/socke.37ab2216.jpg"
+  },
+  {
+    "revision": "577678598bcb47e44f38a361907bcf17",
+    "url": "/shop/static/media/surprise4.57767859.jpg"
+  },
+  {
+    "revision": "1c6e1736e6606eea7af631e5970752e2",
+    "url": "/shop/static/media/tastatur.1c6e1736.jpg"
+  },
+  {
+    "revision": "5347102e189fade877e2bae721be23f0",
+    "url": "/shop/static/media/warnschild.5347102e.jpg"
+  },
+  {
+    "revision": "f83d4b83f471b98ef033d84d9579d82f",
+    "url": "/shop/static/media/weinglas.f83d4b83.jpg"
+  },
+  {
+    "revision": "ca1ed49a1e9394e2ba3fbf8414d8d5dc",
+    "url": "/shop/static/media/zahnbuerste.ca1ed49a.jpg"
+  }
+]);
